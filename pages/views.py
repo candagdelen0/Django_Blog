5a0ca33e81b0a5f350f3c1d1bc9e877a5367016c
candagdelen0 <candagdelen0@gmail.com>
2023-08-28 18:24:30 +0300

    Index Anasayfası sayfası

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Blog, Adviser
 from django.core.paginator import Paginator
+
+def index(request):
+    cities = Blog.objects.all().values()
+    template = loader.get_template('pages/index.html')
+    context = {
+        'cities': cities,
+    }
+    return HttpResponse(template.render(context, request))
