cda4015afbdd14f0b1953ff1e46593a06df54d21
candagdelen0 <candagdelen0@gmail.com>
2023-08-30 16:36:03 +0300

    Öneriler | model - bağlantı kurulumu

--- pages/models.py
+++ pages/models.py
@@ -9,3 +9,12 @@ class Blog(models.Model):
     
     def __str__(self):
         return  f"{self.sehiradi} - {self.ulkeAdi}"
+
+class Adviser(models.Model):
+    baslik = models.CharField(max_length=100)
+    aciklama = models.CharField(max_length=255)
+    metin = models.TextField()
+    resim = models.CharField(max_length=100, default="10.jpg")
+
+    def __str__(self):
+        return f"{self.baslik}"
